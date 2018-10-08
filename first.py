8a67b0c43c18601daf05b06696b87722539aa16d
BABU JOHN <bbuj27@gmail.com>
2018-10-08 16:01:13 +0000

    message

---
+++ first.py
@@ -0,0 +1,167 @@
+# challenge
+#if x is more than 10, all 4 letters are printed
+#otherwise (x is 10 or less)only a and b are printed
+
+#google python if statement
+
+# x=6
+
+# print("a")
+# print("b")
+# if x>10:
+#     print("c")
+#     print("d")
+# print("e")
+ 
+    
+
+
+# challenge
+#if x is more than 10, all 5 letters are printed
+#otherwise (x is 10 or less)only a and b are printed
+
+#google python if  else statement
+
+# x=4
+# if x>10:
+#     print("a")
+#     print("b")
+#     print("c")
+#     print("d")
+#     print("e")
+# else:
+#     print("a")
+#     print("b")
+
+
+# challenge
+# If x is more than 10, print  A, C, D, & E
+# Otherwise (x is 10 or less) print A, B, D, E
+
+# Google Python If Else Statement
+
+# x = 12
+
+# print("A")
+
+# if x <= 10:
+#     print("B")
+# else:
+#     print("C")
+
+# print("D")
+# print("E")
+
+
+# x = 15
+
+# if x % 3 == 0:
+#     if x > 10:
+#         print("A")
+# else:
+#     print("B")
+
+
+# x = 9
+
+# if x % 3 == 0:
+#     if x > 10:
+#         print("A")
+# else:
+#     print("B")
+
+# x = 14
+
+# if x % 3 == 0:
+#     if x > 10:
+#         if x == 15:
+#             print("Ooooh! 15!!!")
+#         else:
+#             print("Boring")
+#     else:
+#         print("B")
+# else:
+#     print("C")
+
+# print("Done")
+    
+
+# x = 15
+
+# if x % 3 == 0:
+#     if x > 10:
+#         if x == 15:
+#             print("Ooooh! 15!!!")
+#         else:
+#             print("Boring")
+#     else:
+#         print("B")
+# else:
+#     print("C")
+
+# print("Done")
+
+
+
+
+# x=0
+
+# while x<3:
+#     print("a")
+#     x+=1
+      
+
+# print("b")
+# print("c")
+# print("d")
+# print("e")
+
+# x=0
+
+# while x<=3:
+#     print("a")
+#     x+=1
+      
+
+# print("b")
+# print("c")
+# print("d")
+# print("e")
+
+
+# x = 5
+
+# print("A")
+
+# n = 0
+# while n < x:
+#     print("B")
+#     n = n + 1
+    
+# print("C")
+# print("D")
+# print
+
+secret = input("enter a number(1>100):")
+secret = int(secret)
+
+while True:
+    guess = input("enter a guess:")
+    guess = int(guess)
+    
+    if guess == secret:
+        print("you got it!")
+        break
+    
+    if guess<secret:
+        print("too low")
+        break
+    
+    if guess>secret:
+        print("too high")
+        
+            
+
+
+
+
